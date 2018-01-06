8449987910df289e2b474011cf9f9155d2550865
Mouthik <mouthik7@gmail.com>
2018-01-06 16:45:26 +0530

    Updated Path Controller

--- src/main/java/hello/IceCreamController.java
+++ src/main/java/hello/IceCreamController.java
@@ -4,29 +4,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 @RestController
 public class IceCreamController {
+    private static final String ERROR = "NO ID FOUND";
     @RequestMapping("/icecream")
     public IceCream iceCream(@RequestParam(value = "id") int id) {
 
-        String error="NO ID FOUND";
-        HashMap<Integer, String> ice = new HashMap<Integer, String>();
-        ice.put(1, "Venilla");
-        ice.put(2, "Choclate");
-        ice.put(3, "StrawBerry");
-        ice.put(4, "BlackCurrent");
-        ice.put(5, "Pista");
+        IceCreamData data = new IceCreamData();
+        HashMap<Integer, String> ice = data.getIcecreams();
 
         if(ice.containsKey(id)){
             return new IceCream(id,ice.get(id));
         }
         else {
-            return new IceCream(id, error);
+            return new IceCream(id, ERROR);
         }
                     
     }
---
+++ src/main/java/hello/IceCreamData.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.HashMap;
+
+public class IceCreamData {
+
+    public HashMap<Integer, String> getIcecreams() {
+        HashMap<Integer, String> ice = new HashMap<Integer, String>();
+        ice.put(1, "Venilla");
+        ice.put(2, "Choclate");
+        ice.put(3, "StrawBerry");
+        ice.put(4, "BlackCurrent");
+        ice.put(5, "Pista");
+        return ice;
+    }
+}
